92c3043f65b842e259aae95d8d67b79a7e040d7e
Skylar Childers <skylarchilders2006@gmail.com>
2026-01-05 19:00:45 -0500

    add country to u8 conversion

--- src/header/location/mod.rs
+++ src/header/location/mod.rs
@@ -10,61 +10,10 @@ pub mod subregion;
 pub struct Location {
     country: Country,
     subregion: Subregion,
-    location_versioning: LocationVersioning,
 }
 
 impl Location {
-    pub fn new(country_id: u8, subregion_id: u8, known_version: Option<LocationVersioning>) {
-        let mut known_version = match known_version {
-            None => LocationVersioning::get_min_from_country_id_number(country_id),
-            Some(v) => v,
-        };
-
-        todo!()
-    }
-
-    pub fn change_version(&mut self, version: LocationVersioning) -> Self {
-        // TODO: Handle changing version
-        self.location_versioning = version;
-        todo!()
-    }
-}
-
-#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub enum LocationVersioning {
-    Vanilla,
-    ExtendedRegionsV1_0,
-    ExtendedRegionsV1_1,
-    ExtendedRegionsV1_2,
-    ExtendedRegionsV1_3, // This is WIP rn
-}
-
-impl LocationVersioning {
-    pub fn get_min_from_country_id_number(value: u8) -> Self {
-        match value {
-            1 | 8..=52 | 64..=121 | 128 | 136 | 144..=145 | 152..=156 | 160 | 168..=177 | 255 => {
-                Self::Vanilla
-            }
-            2..=7
-            | 53..=63
-            | 122..=127
-            | 129..=135
-            | 137..=143
-            | 146..=151
-            | 157..=159
-            | 161..=167
-            | 178..=254 => Self::ExtendedRegionsV1_0,
-            0 => todo!(),
-        }
-    }
-
-    pub fn next(self) -> Option<Self> {
-        match self {
-            Self::Vanilla => Some(Self::ExtendedRegionsV1_0),
-            Self::ExtendedRegionsV1_0 => Some(Self::ExtendedRegionsV1_1),
-            Self::ExtendedRegionsV1_1 => Some(Self::ExtendedRegionsV1_2),
-            Self::ExtendedRegionsV1_2 => Some(Self::ExtendedRegionsV1_3),
-            Self::ExtendedRegionsV1_3 => None,
-        }
+    pub fn new(country_id: u8, subregion_id: u8) {
+        let country = Country::try_from(country_id);
     }
 }
